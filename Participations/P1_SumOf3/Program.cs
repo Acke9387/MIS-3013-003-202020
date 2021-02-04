f80bbcdc2fbb11cde000963cead18287ef0df01a
Ackerman, Adam P <adamackrmn@gmail.com>
2021-02-04 17:34:09 -0600

    Added a solution for participations and made a constant in the first participation

---
+++ Participations/P1_SumOf3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P1_SumOf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double MAGIC_NUMBER = 7.777;//local constant variable
+
+            //MAGIC_NUMBER = 8;
+
+        }
+    }
+}
