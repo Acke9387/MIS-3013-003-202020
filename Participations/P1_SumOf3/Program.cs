219d297cb86af37c99c2c9e0bec728865d2cbbc5
Ackerman, Adam P <adamackrmn@gmail.com>
2021-02-09 17:50:47 -0600

    Finished the particiation of the sum of 3 numbers

--- Participations/P1_SumOf3/Program.cs
+++ Participations/P1_SumOf3/Program.cs
@@ -10,6 +10,26 @@ namespace P1_SumOf3
 
             //MAGIC_NUMBER = 8;
 
+            Console.WriteLine("Please enter the first number >>");
+            string answer = Console.ReadLine();
+            double num1 = Convert.ToDouble(answer);
+
+            Console.WriteLine("Please enter the second number >>");
+            answer = Console.ReadLine();
+            double num2 = Convert.ToDouble(answer);
+
+            Console.WriteLine("Please enter the third number >>");
+            answer = Console.ReadLine();
+            double num3 = Convert.ToDouble(answer);
+
+            double sum = num1 + num2 + num3;
+
+            Console.WriteLine($"{num1} + {num2} + {num3} = {sum.ToString("N3")}");
+
+            double magicNumberTimesSum = sum * MAGIC_NUMBER;
+
+            Console.WriteLine($"Your sum multiplied by our magic number is {magicNumberTimesSum.ToString("N3")}!");
+
         }
     }
 }
