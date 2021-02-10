2b26b1d85484b5dc82d572daefcc8fe212de1c98
Ackerman, Adam P <adamackrmn@gmail.com>
2021-02-09 18:21:52 -0600

    Started the 2nd participation over the coin toss

---
+++ Participations/P2_CoinToss/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace P2_CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNbr = rand.Next(1,3); // generate a random # between 1 and 2
+
+            Console.WriteLine(randomNbr);
+        }
+    }
+}
