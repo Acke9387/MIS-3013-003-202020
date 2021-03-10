c180311c725f777e0f5bfb0891d46dd6596437b9
Ackerman, Adam P <adamackrmn@gmail.com>
2021-03-09 18:21:19 -0600

    Added some problems for midterm review

---
+++ Midterm_Review/IndexOf/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IndexOf
+{
+    class Program
+    {
+        
+        static string msg2;
+        static void Main(string[] args)
+        {
+            //            0123456789...
+            const string msg = "Hello World!";
+
+            double indexOfTheFirstW = msg.IndexOf('W');
+            char middleInitial = 'P';
+
+            msg2 = "Adam";
+
+            Console.WriteLine(msg);
+            Console.WriteLine($"It exists \"{indexOfTheFirstW.ToString("C2")}\"");
+        }
+    }
+}
---
+++ Midterm_Review/TryParse/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = "55s";
+            int realNumber;// = Convert.ToInt32(number);
+            bool b = int.TryParse(number, out realNumber);
+            if (b == true)
+            {
+                Console.WriteLine("Ayyy good #");
+            }
+            else
+            {
+                Console.WriteLine("Ayy, bad #");
+                //return;
+                Environment.Exit(-1);
+            }
+
+            Console.WriteLine(number + 5);
+
+        }
+    }
+}
