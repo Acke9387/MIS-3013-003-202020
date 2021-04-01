48112c267a2218eb8c406c70737a1cab0cc4a574
Ackerman, Adam P <adamackrmn@gmail.com>
2021-04-01 18:38:15 -0500

    started the functions participation

---
+++ Participations/Functions_Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Functions_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static double Add(double v1, double val2)
+        {
+            double sum = v1 + val2;
+
+            return sum;
+
+            return v1 + val2;
+        }
+
+    }
+}
