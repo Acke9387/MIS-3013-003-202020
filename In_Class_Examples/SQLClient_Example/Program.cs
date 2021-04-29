5db0d7cc72d3fa2e28d6b9f1b372404222c4b379
Ackerman, Adam P <adamackrmn@gmail.com>
2021-04-29 17:45:03 -0500

    did an in class example over SQL

---
+++ In_Class_Examples/SQLClient_Example/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace SQLClient_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            string connectionString = "Data Source=tcp:s13.winhost.com;Initial Catalog=DB_128040_practice;User ID=student;Password=AdamTheGreat2020;Integrated Security=False;";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                try
+                {
+                    using (SqlCommand command = new SqlCommand("SELECT StudentID, FirstName, LastName, FavoriteColor " +
+                                                               "FROM Student", con))
+                    {
+                        SqlDataReader reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+                            //Loop through each row returned from the database
+                            int id = Convert.ToInt32(reader["StudentID"]);
+                            string first = reader[1].ToString();
+                            string last = reader["LastName"].ToString();
+                            string color = reader[3].ToString();
+
+                            //Student s = new Student();
+                            //s.First = first;
+                            //s.Last = last;
+                            //s.ID = id;
+                            //s.FavoriteColor = color;
+
+                            if (color.ToLower().Contains("blue") == true)
+                            {
+                                Console.WriteLine($"{first} {last} ({id}) likes {color}");
+
+                            }
+                        }
+                        reader.Close();
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Something went wrong");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ Participations/Classes_Cereal/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Classes_Cereal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Cereal> bowls = new List<Cereal>();
+            string[] lines = File.ReadAllLines("Cereal_Data.txt");
+            //[0] = "name|manufacturer|calories|cups"
+            //[1] = "100% Bran|Nabisco|70|0.33"
+            //[..] = ".."
+
+            // Read in all the data from the file and make Cereal objects and add those to our bowls collection
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                //Line = "100% Bran|Nabisco|70|0.33"
+
+                string[] pieces = line.Split('|');
+                //                0           1       2      3
+                //pieces = { "100% Bran", "Nabisco", "70", "0.33" }
+                Cereal c = new Cereal();
+                c.Name = pieces[0];
+                c.Manufacturer = pieces[1];
+                c.Calories = Convert.ToDouble(pieces[2]);
+                c.Cups =     Convert.ToDouble(pieces[3]);
+
+                bowls.Add(c);
+            }
+
+            // Go through bowls and output the desired cereals
+
+
+        }
+    }
+}
